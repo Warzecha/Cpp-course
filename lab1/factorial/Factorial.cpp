586aa156caaddc38e7383967f1dc2228213e5c6c
MichasPL <torzewicz.michal@gmail.com>
2018-02-27 18:20:25 +0100

    dziala wszystko, tylko gamma wywala blad, ale to winna zaokraglen

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -5,15 +5,17 @@
 #include "Factorial.h"
 #include <cmath>
 
-double factorial(int value) {
+int factorial(int value) {
 
-  if (value == 0 || value == 1 || value == -1)
+  if (value == 0 || value == 1)
     return 1;
-  else return (value<0) ? value * factorial(value-1) : value * factorial(fabs(value)-1);
+  else if (value == -1)
+    return -1;
+  else if (value >= 13) return 0;
+  else return (value>0) ? value * factorial(value-1) : value * factorial(value + 1);
   //if(value == 0)
-   // return 1;
- // else
-  //  return tgamma(fabs(value))*value ? value%2 == 0 && value>0: tgamma(fabs(value))*fabs(value);
-
+  //  return 1;
+  //else
+  //  return tgamma(value)* value;
 
 }
--- lab1/factorial/Factorial.h
+++ lab1/factorial/Factorial.h
@@ -5,6 +5,6 @@
 #ifndef JIMP_EXERCISES_FACTORIAL_H
 #define JIMP_EXERCISES_FACTORIAL_H
 
-double factorial(int value);
+int factorial(int value);
 
 #endif //JIMP_EXERCISES_FACTORIAL_H

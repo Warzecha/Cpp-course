71418e3214275c09d4c090ee6df958294b2ff706
damian <damian12525@gmail.com>
2018-02-27 09:34:28 +0100

    reverse

--- lab1/reversestring/ReverseString.cpp
+++ lab1/reversestring/ReverseString.cpp
@@ -10,9 +10,7 @@ std::string reverse(std::string str)
 
     if(str[0] != '\0')
     {
-        std::string new_str;
-
-        return str.substr(0,str.length()-1) + str[0];
+       return reverse(str.substr(1,str.length())) + str[0];
 
     }
     else return "";
--- lab1/reversestring/main.cpp
+++ lab1/reversestring/main.cpp
@@ -4,4 +4,16 @@
 
 
 #include <iostream>
+#include <cstdlib>
 #include "ReverseString.h"
+
+
+
+
+
+int main(){
+
+    std::cout << reverse("abcd");
+
+    return 0;
+}

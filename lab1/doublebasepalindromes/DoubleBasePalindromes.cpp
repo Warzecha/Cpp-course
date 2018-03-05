e0c5c2ce9f1ea33fcf402969b092298b79726cb8
damian <damian12525@gmail.com>
2018-03-05 22:25:54 +0100

    palindromy i multiplication table

---
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
@@ -0,0 +1,23 @@
+//
+// Created by damian on 05.03.18.
+//
+
+#include "DoubleBasePalindromes.h"
+
+
+uint64_t DoubleBasePalindromes(int max_vaule_exculsive)
+{
+    uint64_t sum = 0;
+
+    for (int i = 1; i < max_vaule_exculsive ; ++i) {
+
+
+
+
+
+    }
+
+
+
+
+}

1c4d4a879acdd69761316eb1bd8b563ea4a8e9b2
damian <damian12525@gmail.com>
2018-03-13 08:38:33 +0100

    greatestproduct step1

--- lab2/greatestproduct/GreatestProduct.cpp
+++ lab2/greatestproduct/GreatestProduct.cpp
@@ -26,61 +26,109 @@ void addNumber(std::vector<int> &numbers, int x)
 
 }
 
+int function (std::vector<int> numbers, int index, int k, int step)
+{
+    if(step == k )
+        return 1;
+    if(index == numbers.size())
+        return 0;
 
+    int a = function(numbers, index+1, k, step);
+    int b = 0;
+
+    //if(index != numbers.size()-1)
+    b = numbers[index] * function(numbers, index+1, k, step+1);
+
+    if(a == 0)
+        return b;
+    else if(b == 0)
+        return a;
+    else return std::max(a, b);
+
+//
+
+
+}
 
 
 int GreatestProduct(const std::vector<int> &numbers, int k) {
 
-    std::vector<int> positive;
-    std::vector<int> negative;
-
-    for( auto x : numbers)
-    {
-        if( x > 0)
-        {
-            addNumber(positive,x);
-        } else
-        {
-            addNumber(negative,x);
-        }
-    }
-
-    std::vector<int> greatest;
-
-    if((negative.size()/2*2) + positive.size() > k)
-    {
-        greatest = positive;
 
 
-        for (int j = 0;  j < int(negative.size())-1 && (j < k - 1); j+=2)
-        {
-
-
-            if(greatest.size() < k - 1 )
-            {
-                addNumber(greatest, -negative[j]);
-                addNumber(greatest, -negative[j+1]);
-            }else if((-negative[j])*(-negative[j+1]) > positive[k-1]*positive[k-2])
-            {
-                addNumber(greatest, -negative[j]);
-                addNumber(greatest, -negative[j+1]);
-            }
-
-        }
-
-
-
-
-    }
-    else
-    {
+    int product;
+    product = function(numbers, 0, k, 0);
+    return product;
 
 
 
 
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    std::vector<int> positive;
+//    std::vector<int> negative;
+//
+//    for( auto x : numbers)
+//    {
+//        if( x > 0)
+//        {
+//            addNumber(positive,x);
+//        } else
+//        {
+//            addNumber(negative,x);
+//        }
+//    }
+//
+//    std::vector<int> greatest;
+//
+//    if((negative.size()/2*2) + positive.size() > k)
+//    {
+//        greatest = positive;
+//
+//
+//        for (int j = 0;  j < int(negative.size())-1 && (j < k - 1); j+=2)
+//        {
+//
+//
+//            if(greatest.size() < k - 1 )
+//            {
+//                addNumber(greatest, -negative[j]);
+//                addNumber(greatest, -negative[j+1]);
+//            }else if((-negative[j])*(-negative[j+1]) > positive[k-1]*positive[k-2])
+//            {
+//                addNumber(greatest, -negative[j]);
+//                addNumber(greatest, -negative[j+1]);
+//            }
+//
+//        }
+//
+//
+//
+//
+//    }
+//    else
+//    {
+//
+//
+//
+//
+//
+//
+//    }
 
 
 
@@ -112,12 +160,15 @@ int GreatestProduct(const std::vector<int> &numbers, int k) {
 //
 //
 //
-    int product = greatest[0];
-    for( auto it = greatest.begin()+1; it != greatest.begin() + k; it++)
-    {
-        product *= *it;
 
-    }
+
+
+//    int product = greatest[0];
+//    for( auto it = greatest.begin()+1; it != greatest.begin() + k; it++)
+//    {
+//        product *= *it;
+//
+//    }
 
 
 
--- lab2/greatestproduct/main.cpp
+++ lab2/greatestproduct/main.cpp
@@ -11,9 +11,9 @@ int main()
 
     std::vector<int> numbers;
 
-    numbers.push_back(-17);
-    numbers.push_back(3);
-    numbers.push_back(-1);
+    numbers.push_back(-2);
+    numbers.push_back(2);
+    numbers.push_back(-3);
 
 
 

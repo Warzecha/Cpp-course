bfa2d2d1dcbcd51389db0da94481ca68c67728ec
damian <damian12525@gmail.com>
2018-03-20 18:28:55 +0100

    simpleURL

--- lab4/netsurl/SimpleUrl.cpp
+++ lab4/netsurl/SimpleUrl.cpp
@@ -2,4 +2,77 @@
 // Created by damian on 20.03.18.
 //
 
+#include <iostream>
 #include "SimpleUrl.h"
+
+nets::SimpleUrl::SimpleUrl(std::string str) {
+
+    std::cmatch m;
+
+
+    //^([A-Za-z]+):(\/{2})?([A-Za-z0-9]*:)?([A-Za-z0-9]*@)?([A-Za-z0-9.]*)(:[0-9]+)?(\/[A-Za-z0-9%\/\.-]+)
+
+    //to dobre
+    //^([A-Za-z]+):(\/{2})?([A-Za-z0-9]*:)?([A-Za-z0-9]*@)?([A-Za-z0-9.-]*)(:[0-9]+)?(\/[A-Za-z0-9%\/\.\,_\@=:!-]*)?(\?[A-Za-z0-9%\/\.\,_\@=!-&]*)?(#[A-Za-z0-9%\/\.\,_\@=!-]*)?
+
+
+    std::regex expresion("^([A-Za-z]+):(\\/{2})?([A-Za-z0-9]*:)?([A-Za-z0-9]*@)?([A-Za-z0-9.-]*)(:[0-9]+)?(\\/[A-Za-z0-9%\\/\\.\\,_\\@=:!-]*)?(\\?[A-Za-z0-9%\\/\\.\\,_\\@=!-&]*)?(#[A-Za-z0-9%\\/\\.\\,_\\@=!-]*)?");
+
+
+    const char * str2 = str.c_str();
+
+    std::regex_match(str2, m, expresion);
+
+    std::string tmp;
+
+    this->scheme = m[1];
+
+    tmp = m[4];
+    tmp = tmp.substr(0, tmp.size()-1);
+    this->login = tmp;
+    this->host = m[5];
+
+    tmp = m[6];
+
+    tmp.erase(0,1);
+    if(tmp.size() > 0)
+        this->port = static_cast<uint16_t>(std::stoi(tmp));
+    else
+    {
+        if(this->scheme == "http")
+            this->port = 80;
+        else if(this->scheme == "https")
+            this->port = 443;
+        else if(this->scheme == "ftp")
+            this->port = 22;
+        else this->port = 0;
+    }
+
+    tmp = m[7];
+    tmp.erase(0,1);
+    this->path = tmp;
+
+
+    tmp = m[8];
+    tmp.erase(0,1);
+    this->query = tmp;
+
+    tmp = m[9];
+    tmp.erase(0,1);
+    this->fragment = tmp;
+
+
+    //delete(str2);
+
+}
+
+nets::SimpleUrl::~SimpleUrl() {
+//    delete(&scheme);
+//    delete(&login);
+//    delete(&fragment);
+//    delete(&query);
+//    delete(&port);
+//    delete(&host);
+
+
+}
--- lab4/netsurl/SimpleUrl.h
+++ lab4/netsurl/SimpleUrl.h
@@ -6,9 +6,67 @@
 #define JIMP_EXERCISES_SIMPLEURL_H
 
 
-class SimpleUrl {
+#include <cstdint>
+#include <string>
+#include <regex>
 
-};
+
+namespace nets {
+
+    class SimpleUrl {
+    public:
+
+        SimpleUrl(std::string str);
+
+
+
+
+        std::string Login() const {
+            return this->login;
+        }
+
+        std::string Host() const {
+            return this->host;
+        }
+
+        std::string Path() const {
+            return this->path;
+        }
+
+        uint16_t Port() const {
+            return this->port;
+        }
+
+        std::string Scheme() const {
+            return this->scheme;
+        }
+
+        std::string Query() const {
+            return this->query;
+        }
+
+        std::string Fragment() const {
+            return this->fragment;
+        }
+
+        virtual ~SimpleUrl();
+
+
+    private:
+
+        std::string login;
+        std::string host;
+        std::string path;
+        uint16_t port;
+        std::string scheme;
+        std::string query;
+        std::string fragment;
+
+
+
+    };
+
+}
 
 
 #endif //JIMP_EXERCISES_SIMPLEURL_H
